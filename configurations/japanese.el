d3179809104d8e46589052aca5eaf5d04ccec717
root <root@localhost.localdomain>
2014-12-02 00:50:48 +0900

    Avoid font loading error on platforms other than mac

--- configurations/japanese.el
+++ configurations/japanese.el
@@ -4,12 +4,15 @@
 
 ;; フォントの設定
 ;; 出典：http://sakito.jp/emacs/emacs23.html
-(when (fboundp 'create-fontset-from-ascii-font)
-  (create-fontset-from-ascii-font "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
-  (set-fontset-font "fontset-menlokakugo"
-		    'unicode
-		    (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
-		    nil
-		    'append)
-  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo")))
-
+(cond
+ ((string-match "apple-darwin" system-configuration)
+  ;; only on mac
+  (when (fboundp 'create-fontset-from-ascii-font)
+    (create-fontset-from-ascii-font "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
+    (set-fontset-font "fontset-menlokakugo"
+                      'unicode
+                      (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
+                      nil
+                      'append)
+    (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo")))
+  ))
